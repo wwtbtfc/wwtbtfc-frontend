5a262bb6ff46d6fff50f3ee596e1d03cb9ae08e2
Wes Heginbotham <wesheg@gmail.com>
2024-02-21 22:24:36 -0800

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@ui/styles/index.css';
 import { HomePage } from '@ui/views';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
