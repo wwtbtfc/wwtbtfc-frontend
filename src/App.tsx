83a6af5bf35880eea875a405373d39b170aaa5fe
Wes Heginbotham <wesheg@gmail.com>
2024-02-21 22:06:56 -0800

    feat: set light and dark buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,26 @@ import './App.css';
 import { Button } from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   return (
     <>
       <div>
         <img src={logo} className="logo" alt="WWTBTFC logo" />
       </div>
-      <h1>Game Placeholder</h1>
+      <h1>Let's Make a Game!</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <Button onClick={() => setCount1((count1) => count1 + 1)}>
+          Light Button clicked {count1} times
+        </Button>
+      </div>
+      <div className="card">
+        <Button
+          variant="dark"
+          onClick={() => setCount2((count2) => count2 + 1)}
+        >
+          Dark Button clicked {count2} times
         </Button>
       </div>
     </>
