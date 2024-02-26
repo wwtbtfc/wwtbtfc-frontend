5298dfd44ed1774b2a1de62da1344eba13f844f9
brodieheg <brodieheg@gmail.com>
2024-02-25 21:23:52 -0500

    chore: added Type for props

--- src/components/TitleBanner/TitleBanner.tsx
+++ src/components/TitleBanner/TitleBanner.tsx
@@ -1,14 +1,16 @@
 import StyledTitleBanner from './StyledTitleBanner';
+import { AppProps } from '@ui/Types/AppProps';
 
 /**
  * Title Banner component
  * @param {string} props.title - pass the desired title to the banner
  */
-export const TitleBanner = (title: string) => {
+export const TitleBanner = (props: AppProps) => {
   return (
     <>
       <StyledTitleBanner>
-        <h2 className="title">{title}</h2>
+        <h2 className="title">{props.title}</h2>
+        <img src="../../assets/UserIcon.svg" />
         <h5 className="user">username</h5>
       </StyledTitleBanner>
     </>
---
+++ src/components/TitleBanner/index.ts
@@ -0,0 +1,1 @@
+export { TitleBanner } from './TitleBanner';
