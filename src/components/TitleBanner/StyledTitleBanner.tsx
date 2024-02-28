416432126903cabd0e51bae5e4f5889e604ecdc8
brodieheg <brodieheg@gmail.com>
2024-02-28 16:11:21 -0500

    fix: changed position absolute to relative, aligned items

--- src/components/TitleBanner/StyledTitleBanner.tsx
+++ src/components/TitleBanner/StyledTitleBanner.tsx
@@ -2,38 +2,34 @@ import styled from 'styled-components';
 
 const StyledTitleBanner = styled.header`
   margin: 0 auto;
-  text-align: center;
   display: flex;
-  justify-content: space-between;
+  flex-direction: row;
   align-items: center;
+  justify-content: center;
   height: 90px;
   border-bottom: 2px solid #4d2f2e;
-  min-width: 1200px;
 
   .title {
     font-size: 3em;
     flex-shrink: 0;
     padding: 1.5em;
     margin: 0 auto;
-    text-align: center;
-    display: inline-block;
-    position: absolute;
-    left: 50%;
+    position: relative;
+    left: 5%;
     min-width: 200px;
-    transform: translateX(-50%);
   }
   .user {
     font-family: Arial;
+    position: relative;
     overflow: hidden;
     flex-shrink: 0;
     font-size: 1.5em;
     font-weight: 100;
-    position: absolute;
-    right: 3em;
+    right: 2em;
     display: inline block;
   }
   img {
-    position: absolute;
+    position: relative;
     flex-shrink: 0;
     right: 15px;
     max-width: 35px;
