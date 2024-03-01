4a12d61aec0752b90e367c2dc9fccc93c3a4154c
Brodie Heginbotham <111663689+brodieheg@users.noreply.github.com>
2024-03-01 08:50:17 -0500

    Update StyledTitleBanner.tsx

--- src/components/TitleBanner/StyledTitleBanner.tsx
+++ src/components/TitleBanner/StyledTitleBanner.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import colors from '@ui/styles/colors';
 
 const StyledTitleBanner = styled.header`
   margin: 0 auto;
   display: flex;
   align-items: center;
   justify-content: center;
-  border-bottom: 2px solid #4d2f2e;
+  border-bottom: 2px solid ${colors.brown};
   min-width: 560px;
 
   .title {
