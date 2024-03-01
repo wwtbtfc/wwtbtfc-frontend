bf81f9bb7677361866c24eb229bb2723e86e4ca5
brodieheg <brodieheg@gmail.com>
2024-03-01 15:13:56 -0500

    feat: back button styles

--- src/components/BackButton/StyledBackButton.tsx
+++ src/components/BackButton/StyledBackButton.tsx
@@ -1,6 +1,21 @@
 import { Button } from '..';
 import styled from 'styled-components';
+import BackLight from '../../views/HowToPlay/assets/BackLight.svg';
+import colors from '@ui/styles/colors';
 
 export const StyledBackButton = styled(Button)`
-  color: #ff0000;
+  border-radius: 5px;
+  color: ${colors.tan};
+  font-size: 20px;
+  background-image: url(${BackLight});
+  background-size: 1.25em;
+  background-repeat: no-repeat;
+  background-position-y: center;
+  background-position-x: 0.5em;
+  padding-left: 2.5em;
+  padding-right: 1em;
+  position: relative;
+  margin-top: 5vh;
+  left: 3vw;
+  cursor: pointer;
 `;
