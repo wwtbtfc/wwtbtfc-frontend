42c67129039c8a5a06ac842350e5cd0667f9fad8
Brodie Heginbotham <111663689+brodieheg@users.noreply.github.com>
2024-03-02 12:06:14 -0500

    Update src/components/BackButton/BackButton.tsx
    Co-authored-by: wesheg <60304565+wesheg@users.noreply.github.com>

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -7,6 +7,16 @@ export interface BackButtonProps
   text?: string;
   path?: string;
 }
+/**
+ * Clicking this button will bring the user back to the given `path`.
+ * This component is only meant to be used for menu navigation before the host has started a game.
+ *
+ * @param {object} props - Props object for the component
+ * @param {string} props.text - Optional; The text label to appear inside the button
+ *        Defaults to "Back To Home"
+ * @param {string} props.path - Optional; The relative URL path to which the user will travel on click.
+ *        Defaults to "/"
+ */
 export const BackButton = ({
   text = 'Back To Home',
   path = '/',
