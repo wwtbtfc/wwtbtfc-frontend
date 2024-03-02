d3810651053c113fe4e486d4e09b090dbf008570
brodieheg <brodieheg@gmail.com>
2024-03-02 15:56:26 -0500

    feat: dynamic button text

--- src/components/KeepReading/KeepReading.tsx
+++ src/components/KeepReading/KeepReading.tsx
@@ -1,11 +1,32 @@
+'use client';
 import { StyledKeepReading } from './StyledKeepReading';
 import { Button } from '..';
 
 export const KeepReading = () => {
+  interface possibleButtons {
+    path: string;
+    title: string;
+  }
+  const possibleButtons: possibleButtons[] = [
+    { path: '/how-to-play', title: 'Overview' },
+    { path: '/hosts', title: 'Hosts' },
+    { path: '/contestants', title: 'Contestants' },
+    { path: '/scoring', title: 'Scoring' },
+    { path: '/lifelines', title: 'Lifelines' },
+  ];
+
+  const url = window.location.href;
+  const ButtonsMap = () => {
+    return possibleButtons.map((button) => {
+      if (!url.includes(button.path)) {
+        return <Button className="button">{button.title}</Button>;
+      }
+    });
+  };
   return (
     <StyledKeepReading>
       <div className="heading">Keep Reading About: </div>
-      <Button className="button">This button</Button>
+      <div className="button-container">{ButtonsMap()}</div>
     </StyledKeepReading>
   );
 };
--- src/components/KeepReading/StyledKeepReading.tsx
+++ src/components/KeepReading/StyledKeepReading.tsx
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 export const StyledKeepReading = styled.div`
+  display: flex;
+  border-top: 2px dotted gray;
+  position: relative;
+  padding-top: 1.5em;
   .heading {
     font-size: 2em;
   }
   .button {
     border-radius: 10px;
+    flex: 40%;
+    margin: 0 0.5em 0.5em 0.5em;
+  }
+  .button-container {
+    display: flex;
+    flex-wrap: wrap;
   }
 `;
