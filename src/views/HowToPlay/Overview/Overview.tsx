269aff994d3af0cc4cf4c36993d1d62abc1d9faf
brodieheg <brodieheg@gmail.com>
2024-06-10 07:50:19 -0400

    fix: updated layout name for clarification

--- src/views/HowToPlay/Overview/Overview.tsx
+++ src/views/HowToPlay/Overview/Overview.tsx
@@ -1,20 +1,10 @@
-import { useNavigate } from 'react-router';
 import { TitleBanner } from '@ui/components/TitleBanner';
 
 export const Overview = () => {
-  const navigate = useNavigate();
-
   return (
     <>
       <TitleBanner title="How To Play"></TitleBanner>
       <h1>How To Play</h1>
-      <div
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        back to home
-      </div>
     </>
   );
 };
