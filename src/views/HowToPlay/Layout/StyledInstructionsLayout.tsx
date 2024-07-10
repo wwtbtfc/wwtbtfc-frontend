5d517713551197a5a524304f351cc032920c10e3
brodieheg <brodieheg@gmail.com>
2024-07-10 10:12:36 -0400

    style: moved styles up a level from Overview

--- src/views/HowToPlay/Layout/StyledInstructionsLayout.tsx
+++ src/views/HowToPlay/Layout/StyledInstructionsLayout.tsx
@@ -1,6 +1,20 @@
 import styled from 'styled-components';
 
 const StyledInstructionsLayout = styled.div`
+  .container {
+    display: flex;
+    align-items: center;
+    margin: 25px 0 0 3em;
+  }
+
+  h3 {
+    margin: 0;
+    &.highlight-text {
+      margin-left: 0.25em;
+    }
+    justify-content: center;
+    align-items: center;
+  }
   margin-left: 3em;
   margin-right: 3em;
   margin-bottom: 3em;
--- src/views/HowToPlay/Overview/StyledOverview.tsx
+++ src/views/HowToPlay/Overview/StyledOverview.tsx
@@ -1,20 +1,6 @@
 import styled from 'styled-components';
 
 const StyledOverview = styled.div`
-  .container {
-    display: flex;
-    align-items: center;
-    margin: 25px 0 0 3em;
-  }
-
-  h3 {
-    margin: 0;
-    &.highlight-text {
-      margin-left: 0.25em;
-    }
-    justify-content: center;
-    align-items: center;
-  }
   img {
     width: 3em;
     &.image-right {
