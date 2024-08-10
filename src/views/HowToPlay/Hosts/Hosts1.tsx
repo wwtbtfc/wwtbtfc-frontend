51536f7c3e714c6903e0f44389f58d77d7ae5500
brodieheg <brodieheg@gmail.com>
2024-08-10 11:10:08 -0400

    fix: moved TitleBanners up a layer to avoid repitition

--- src/views/HowToPlay/Hosts/Hosts1.tsx
+++ src/views/HowToPlay/Hosts/Hosts1.tsx
@@ -1,4 +1,3 @@
-import { TitleBanner } from '@ui/components/TitleBanner';
 import { StyledInstructionsBody } from '../Layout';
 import { PageArrows } from '@ui/components/PageArrows/PageArrows';
 // todo: reuse the styled coponents to prevent all the wrapping
@@ -6,7 +5,6 @@ import { PageArrows } from '@ui/components/PageArrows/PageArrows';
 export const Hosts1 = () => {
   return (
     <>
-      <TitleBanner title="How To Play"></TitleBanner>
       <StyledInstructionsBody>
         <div id="welcome" className="container">
           Hosts1
--- src/views/HowToPlay/Layout/InstructionsLayout.tsx
+++ src/views/HowToPlay/Layout/InstructionsLayout.tsx
@@ -2,11 +2,13 @@ import { KeepReading, BackButton } from '@ui/components';
 import { Outlet } from 'react-router';
 import StyledInstructionsLayout from './StyledInstructionsLayout';
 import { StyledInstructionsBody } from './StyledInstructionsBody';
+import { TitleBanner } from '@ui/components';
 
 export const InstructionsLayout = () => {
   return (
     <>
       <StyledInstructionsLayout>
+        <TitleBanner title="How To Play"></TitleBanner>
         <Outlet />
       </StyledInstructionsLayout>
       <KeepReading
